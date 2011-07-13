1820960598649f245b265811a05580c010f32b64
Ross Nordby <Ross Nordby@localhost>
2011-07-13 17:39:47 -0500

    GravitationalField now protects against singularities.

--- BEPUphysicsDemos/BEPUphysicsDemos/Sample Code/Force Fields/GravitationalField.cs
+++ BEPUphysicsDemos/BEPUphysicsDemos/Sample Code/Force Fields/GravitationalField.cs
@@ -3,6 +3,7 @@ using BEPUphysics.BroadPhaseSystems;
 using BEPUphysics.Entities;
 using BEPUphysics.UpdateableSystems.ForceFields;
 using Microsoft.Xna.Framework;
+using BEPUphysics;
 
 namespace BEPUphysicsDemos.SampleCode
 {
@@ -52,15 +53,25 @@ namespace BEPUphysicsDemos.SampleCode
         {
             Vector3 r = e.Position - Origin;
             float length = r.Length();
-            float force = dt * e.Mass * Math.Min(MaxAcceleration, Multiplier / (length * length));
-            impulse = -(force / length) * r; //Extra division by length normalizes the direction.
+            if (length > Toolbox.BigEpsilon)
+            {
+                float force = dt * e.Mass * Math.Min(MaxAcceleration, Multiplier / (length * length));
+                impulse = -(force / length) * r; //Extra division by length normalizes the direction.
+            }
+            else
+                impulse = new Vector3();
 
 
             ////Could use a linear dropoff for a slightly faster calculation (divide by length^2 instead of length^3).
             //Vector3 r = e.Position - Origin;
             //float length = r.Length();
+            //if (length > Toolbox.BigEpsilon)
+            //{
             //float force = dt * e.Mass * Math.Min(MaxAcceleration, Multiplier / length);
             //impulse = -(force / length) * r; //Extra division by length normalizes the direction.
+            //}
+            //else
+            //    impulse = new Vector3();
         }
     }
 }
