bfd5a2bb6d4288ce445950985410257a607e603b
Ross Nordby <Ross Nordby@localhost>
2011-09-04 15:34:49 -0500

    Added XML documentation to replacement math types.

--- BEPUphysics/MathExtensions/Quaternion.cs
+++ BEPUphysics/MathExtensions/Quaternion.cs
@@ -10,11 +10,33 @@ namespace BEPUphysics.MathExtensions
     /// </summary>
     public struct Quaternion
     {
+        /// <summary>
+        /// X component of the quaternion.
+        /// </summary>
         public float X;
+
+        /// <summary>
+        /// Y component of the quaternion.
+        /// </summary>
         public float Y;
+
+        /// <summary>
+        /// Z component of the quaternion.
+        /// </summary>
         public float Z;
+
+        /// <summary>
+        /// W component of the quaternion.
+        /// </summary>
         public float W;
 
+        /// <summary>
+        /// Constructs a new Quaternion.
+        /// </summary>
+        /// <param name="x">X component of the quaternion.</param>
+        /// <param name="y">Y component of the quaternion.</param>
+        /// <param name="z">Z component of the quaternion.</param>
+        /// <param name="w">W component of the quaternion.</param>
         public Quaternion(float x, float y, float z, float w)
         {
             this.X = x;
@@ -23,6 +45,12 @@ namespace BEPUphysics.MathExtensions
             this.W = w;
         }
 
+        /// <summary>
+        /// Adds two quaternions together.
+        /// </summary>
+        /// <param name="a">First quaternion to add.</param>
+        /// <param name="b">Second quaternion to add.</param>
+        /// <param name="result">Sum of the addition.</param>
         public static void Add(ref Quaternion a, ref Quaternion b, out Quaternion result)
         {
             result.X = a.X + b.X;
@@ -31,6 +59,12 @@ namespace BEPUphysics.MathExtensions
             result.W = a.W + b.W;
         }
 
+        /// <summary>
+        /// Multiplies two quaternions.
+        /// </summary>
+        /// <param name="a">First quaternion to multiply.</param>
+        /// <param name="b">Second quaternion to multiply.</param>
+        /// <param name="result">Product of the multiplication.</param>
         public static void Multiply(ref Quaternion a, ref Quaternion b, out Quaternion result)
         {
             float x = a.X;
@@ -47,6 +81,12 @@ namespace BEPUphysics.MathExtensions
             result.W = w * bW - x * bX - y * bY - z * bZ;
         }
 
+        /// <summary>
+        /// Scales a quaternion.
+        /// </summary>
+        /// <param name="q">Quaternion to multiply.</param>
+        /// <param name="scale">Amount to multiply each component of the quaternion by.</param>
+        /// <param name="result">Scaled quaternion.</param>
         public static void Multiply(ref Quaternion q, float scale, out Quaternion result)
         {
             result.X = q.X * scale;
@@ -55,6 +95,12 @@ namespace BEPUphysics.MathExtensions
             result.W = q.W * scale;
         }
 
+        /// <summary>
+        /// Multiplies two quaternions together in opposite order.
+        /// </summary>
+        /// <param name="a">First quaternion to multiply.</param>
+        /// <param name="b">Second quaternion to multiply.</param>
+        /// <param name="result">Product of the multiplication.</param>
         public static void Concatenate(ref Quaternion a, ref Quaternion b, out Quaternion result)
         {
             float aX = a.X;
@@ -71,7 +117,9 @@ namespace BEPUphysics.MathExtensions
             result.W = w * aW - x * aX - y * aY - z * aZ;
         }
 
-
+        /// <summary>
+        /// Quaternion representing the identity transform.
+        /// </summary>
         public static Quaternion Identity
         {
             get
@@ -80,6 +128,12 @@ namespace BEPUphysics.MathExtensions
             }
         }
 
+
+        /// <summary>
+        /// Creates a quaternion from a rotation matrix.
+        /// </summary>
+        /// <param name="r">Rotation matrix used to create a new quaternion.</param>
+        /// <returns>Quaternion representing the same rotation as the matrix.</returns>
         public static Quaternion CreateFromRotationMatrix(Matrix r)
         {
             Quaternion toReturn;
@@ -87,6 +141,11 @@ namespace BEPUphysics.MathExtensions
             return toReturn;
         }
 
+        /// <summary>
+        /// Creates a quaternion from a rotation matrix.
+        /// </summary>
+        /// <param name="r">Rotation matrix used to create a new quaternion.</param>
+        /// <param name="q">Quaternion representing the same rotation as the matrix.</param>
         public static void CreateFromRotationMatrix(ref Matrix r, out Quaternion q)
         {
             float tr = r.M11 + r.M22 + r.M33;
@@ -128,6 +187,11 @@ namespace BEPUphysics.MathExtensions
             }
         }
 
+        /// <summary>
+        /// Ensures the quaternion has unit length.
+        /// </summary>
+        /// <param name="quaternion">Quaternion to normalize.</param>
+        /// <returns>Normalized quaternion.</returns>
         public static Quaternion Normalize(Quaternion quaternion)
         {
             Quaternion toReturn;
@@ -135,6 +199,11 @@ namespace BEPUphysics.MathExtensions
             return toReturn;
         }
 
+        /// <summary>
+        /// Ensures the quaternion has unit length.
+        /// </summary>
+        /// <param name="quaternion">Quaternion to normalize.</param>
+        /// <param name="toReturn">Normalized quaternion.</param>
         public static void Normalize(ref Quaternion quaternion, out Quaternion toReturn)
         {
             float inverse = (float)(1 / Math.Sqrt(quaternion.X * quaternion.X + quaternion.Y * quaternion.Y + quaternion.Z * quaternion.Z + quaternion.W * quaternion.W));
@@ -144,6 +213,9 @@ namespace BEPUphysics.MathExtensions
             toReturn.W = quaternion.W * inverse;
         }
 
+        /// <summary>
+        /// Scales the quaternion such that it has unit length.
+        /// </summary>
         public void Normalize()
         {
             float inverse = (float)(1 / Math.Sqrt(X * X + Y * Y + Z * Z + W * W));
@@ -153,12 +225,26 @@ namespace BEPUphysics.MathExtensions
             W *= inverse;
         }
 
+        /// <summary>
+        /// Blends two quaternions together to get an intermediate state.
+        /// </summary>
+        /// <param name="start">Starting point of the interpolation.</param>
+        /// <param name="end">Ending point of the interpolation.</param>
+        /// <param name="interpolationAmount">Amount of the end point to use.</param>
+        /// <returns>Interpolated intermediate quaternion.</returns>
         public static Quaternion Slerp(Quaternion start, Quaternion end, float interpolationAmount)
         {
             Quaternion toReturn;
             Slerp(ref start, ref end, interpolationAmount, out toReturn);
             return toReturn;
         }
+        /// <summary>
+        /// Blends two quaternions together to get an intermediate state.
+        /// </summary>
+        /// <param name="start">Starting point of the interpolation.</param>
+        /// <param name="end">Ending point of the interpolation.</param>
+        /// <param name="interpolationAmount">Amount of the end point to use.</param>
+        /// <param name="result">Interpolated intermediate quaternion.</param>
         public static void Slerp(ref Quaternion start, ref Quaternion end, float interpolationAmount, out Quaternion result)
         {
             double cosHalfTheta = start.W * end.W + start.X * end.X + start.Y * end.Y + start.Z * end.Z;
@@ -209,6 +295,11 @@ namespace BEPUphysics.MathExtensions
 
         }
 
+        /// <summary>
+        /// Computes the conjugate of the quaternion.
+        /// </summary>
+        /// <param name="quaternion">Quaternion to conjugate.</param>
+        /// <returns>Conjugated quaternion.</returns>
         public static Quaternion Conjugate(Quaternion quaternion)
         {
             Quaternion toReturn;
@@ -216,6 +307,11 @@ namespace BEPUphysics.MathExtensions
             return toReturn;
         }
 
+        /// <summary>
+        /// Computes the conjugate of the quaternion.
+        /// </summary>
+        /// <param name="quaternion">Quaternion to conjugate.</param>
+        /// <param name="result">Conjugated quaternion.</param>
         public static void Conjugate(ref Quaternion quaternion, out Quaternion result)
         {
             result.X = -quaternion.X;
@@ -224,16 +320,34 @@ namespace BEPUphysics.MathExtensions
             result.W = quaternion.W;
         }
 
+        /// <summary>
+        /// Tests components for equality.
+        /// </summary>
+        /// <param name="a">First quaternion to test for equivalence.</param>
+        /// <param name="b">Second quaternion to test for equivalence.</param>
+        /// <returns>Whether or not the quaternions' components were equal.</returns>
         public static bool operator ==(Quaternion a, Quaternion b)
         {
             return a.X == b.X && a.Y == b.Y && a.Z == b.Z && a.W == b.W;
         }
 
+        /// <summary>
+        /// Tests components for inequality.
+        /// </summary>
+        /// <param name="a">First quaternion to test for equivalence.</param>
+        /// <param name="b">Second quaternion to test for equivalence.</param>
+        /// <returns>Whether the quaternions' components were not equal.</returns>
         public static bool operator !=(Quaternion a, Quaternion b)
         {
             return a.X != b.X || a.Y != b.Y || a.Z != b.Z || a.W != b.W;
         }
 
+        /// <summary>
+        /// Multiplies two quaternions.
+        /// </summary>
+        /// <param name="a">First quaternion to multiply.</param>
+        /// <param name="b">Second quaternion to multiply.</param>
+        /// <returns>Product of the multiplication.</returns>
         public static Quaternion operator *(Quaternion a, Quaternion b)
         {
             Quaternion toReturn;
@@ -241,6 +355,12 @@ namespace BEPUphysics.MathExtensions
             return toReturn;
         }
 
+        /// <summary>
+        /// Creates a quaternion from an axis and angle.
+        /// </summary>
+        /// <param name="axis">Axis of rotation.</param>
+        /// <param name="angle">Angle to rotate around the axis.</param>
+        /// <returns>Quaternion representing the axis and angle rotation.</returns>
         public static Quaternion CreateFromAxisAngle(Vector3 axis, float angle)
         {
             float halfAngle = angle * .5f;
@@ -253,6 +373,12 @@ namespace BEPUphysics.MathExtensions
             return q;
         }
 
+        /// <summary>
+        /// Creates a quaternion from an axis and angle.
+        /// </summary>
+        /// <param name="axis">Axis of rotation.</param>
+        /// <param name="angle">Angle to rotate around the axis.</param>
+        /// <param name="p">Quaternion representing the axis and angle rotation.</param>
         public static void CreateFromAxisAngle(ref Vector3 axis, float angle, out Quaternion q)
         {
             float halfAngle = angle * .5f;
@@ -263,6 +389,13 @@ namespace BEPUphysics.MathExtensions
             q.W = (float)Math.Cos(halfAngle);
         }
 
+        /// <summary>
+        /// Constructs a quaternion from yaw, pitch, and roll.
+        /// </summary>
+        /// <param name="yaw">Yaw of the rotation.</param>
+        /// <param name="pitch">Pitch of the rotation.</param>
+        /// <param name="roll">Roll of the rotation.</param>
+        /// <returns>Quaternion representing the yaw, pitch, and roll.</returns>
         public static Quaternion CreateFromYawPitchRoll(float yaw, float pitch, float roll)
         {
             Quaternion toReturn;
@@ -270,6 +403,13 @@ namespace BEPUphysics.MathExtensions
             return toReturn;
         }
 
+        /// <summary>
+        /// Constructs a quaternion from yaw, pitch, and roll.
+        /// </summary>
+        /// <param name="yaw">Yaw of the rotation.</param>
+        /// <param name="pitch">Pitch of the rotation.</param>
+        /// <param name="roll">Roll of the rotation.</param>
+        /// <param name="q">Quaternion representing the yaw, pitch, and roll.</param>
         public static void CreateFromYawPitchRoll(float yaw, float pitch, float roll, out Quaternion q)
         {
             double cosYaw = Math.Cos(yaw * .5f);
