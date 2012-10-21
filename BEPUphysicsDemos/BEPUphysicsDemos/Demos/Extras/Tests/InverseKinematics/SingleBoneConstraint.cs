5129709eb8807d454342c18fb8946be8cb08041c
Ross Nordby <Ross Nordby@localhost>
2012-10-20 21:44:34 -0500

    -SingleBoneConstraints now properly use the same effective mass inversion as IKJoints to avoid singularities when handling non-3DOF constraints.

--- BEPUphysicsDemos/BEPUphysicsDemos/Demos/Extras/Tests/InverseKinematics/SingleBoneConstraint.cs
+++ BEPUphysicsDemos/BEPUphysicsDemos/Demos/Extras/Tests/InverseKinematics/SingleBoneConstraint.cs
@@ -45,12 +45,15 @@ namespace BEPUphysicsDemos.Demos.Extras.Tests.InverseKinematics
 
             //Incorporate the constraint softness into the effective mass denominator. This pushes the matrix away from singularity.
             //Softness will also be incorporated into the velocity solve iterations to complete the implementation.
-            effectiveMass.M11 += Softness;
-            effectiveMass.M22 += Softness;
-            effectiveMass.M33 += Softness;
+            if (effectiveMass.M11 != 0)
+                effectiveMass.M11 += Softness;
+            if (effectiveMass.M22 != 0)
+                effectiveMass.M22 += Softness;
+            if (effectiveMass.M33 != 0)
+                effectiveMass.M33 += Softness;
 
-            //Invert! Takes us from J * M^-1 * JT to 1 / (J * M^-1 * JT). 
-            Matrix3X3.Invert(ref effectiveMass, out effectiveMass);
+            //Invert! Takes us from J * M^-1 * JT to 1 / (J * M^-1 * JT).
+            Matrix3X3.AdaptiveInvert(ref effectiveMass, out effectiveMass);
 
         }
 
