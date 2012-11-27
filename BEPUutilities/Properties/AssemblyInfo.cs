06cf647c1f9585172ebd4e2a7c820a15ce86ae84
Ross Nordby <Ross Nordby@localhost>
2012-11-27 17:32:23 -0600

    -Updated some project properties. -Added Vector4 to BEPUutilities.

--- BEPUutilities/Properties/AssemblyInfo.cs
+++ BEPUutilities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BEPUmath")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BEPUutilities")]
+[assembly: AssemblyDescription("Extensions and helpers used by various bepu things.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BEPUmath")]
+[assembly: AssemblyCompany("Bepu Entertainment LLC.")]
+[assembly: AssemblyProduct("BEPUutilities")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
