83681d06fa7c881da4b8664a5f3bb5d166ddd5f3
Ross Nordby <Ross Nordby@localhost>
2013-02-13 16:12:25 -0600

    -Member count and deactivation candidate count are now exposed on the SimulationIsland.

--- BEPUphysics/DeactivationManagement/SimulationIsland.cs
+++ BEPUphysics/DeactivationManagement/SimulationIsland.cs
@@ -38,7 +38,23 @@ namespace BEPUphysics.DeactivationManagement
             }
         }
         internal int memberCount;
+
+        /// <summary>
+        /// Gets the number of simulation island members within this simulation island.
+        /// </summary>
+        public int MemberCount
+        {
+            get { return memberCount; }
+        }
+
         internal int deactivationCandidateCount;
+        /// <summary>
+        /// Gets the number of simulation island members in the simulation island which are prepared to go to sleep.
+        /// </summary>
+        public int DeactivationCandidateCount
+        {
+            get { return deactivationCandidateCount; }
+        }
 
         ///<summary>
         /// Constructs a simulation island.
