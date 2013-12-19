80792a937e1fcf8dc18cff861beb98cac11d0dfd
Ross Nordby <Ross Nordby@localhost>
2013-12-18 20:11:56 -0600

    -Fixed a bug in the DeactivationManager which would sometimes result in unconnected objects being in the same island after being added and removed from the space. -Made the AddRemoveStressDemo a little stronger.

--- BEPUphysicsDemos/BEPUphysicsDemos/Demos/Extras/Tests/AddRemoveStressDemo.cs
+++ BEPUphysicsDemos/BEPUphysicsDemos/Demos/Extras/Tests/AddRemoveStressDemo.cs
@@ -1,10 +1,13 @@
-﻿using BEPUphysics.Entities.Prefabs;
+﻿using BEPUphysics.Entities;
+using BEPUphysics.Entities.Prefabs;
 using System;
 using BEPUphysics.CollisionShapes;
 using BEPUphysics.CollisionShapes.ConvexShapes;
 using System.Collections.Generic;
 using BEPUphysics.NarrowPhaseSystems;
+using BEPUphysics.Threading;
 using BEPUutilities;
+using BEPUutilities.DataStructures;
 
 namespace BEPUphysicsDemos.Demos.Extras.Tests
 {
@@ -15,9 +18,9 @@ namespace BEPUphysicsDemos.Demos.Extras.Tests
     {
 
 
-        float width = 15;
-        float height = 15;
-        float length = 15;
+        float width = 15f;
+        float height = 15f;
+        float length = 15f;
         /// <summary>
         /// Constructs a new demo.
         /// </summary>
@@ -25,7 +28,6 @@ namespace BEPUphysicsDemos.Demos.Extras.Tests
         public AddRemoveStressDemo(DemosGame game)
             : base(game)
         {
-
             NarrowPhaseHelper.Factories.BoxBox.EnsureCount(5000);
             NarrowPhaseHelper.Factories.CompoundCompound.EnsureCount(5000);
             Space.Remove(vehicle.Vehicle);
@@ -33,17 +35,21 @@ namespace BEPUphysicsDemos.Demos.Extras.Tests
 
             for (int i = 0; i < 1000; i++)
             {
+
+                var position = new Vector3(
+                    (float)(random.NextDouble() - 0.5) * width,
+                    (float)(random.NextDouble() - 0.5) * height,
+                    (float)(random.NextDouble() - 0.5) * length);
                 var toAdd =
                     new CompoundBody(new List<CompoundShapeEntry>
                     {
-                        new CompoundShapeEntry(new BoxShape(1,1,1), 
-                        new Vector3(
-                            (float)random.NextDouble() * width,
-                            (float)random.NextDouble() * height,
-                            (float)random.NextDouble() * length), 1)
+                        new CompoundShapeEntry(new BoxShape(1,1,1), position, 1)
                     }, 10);
+                //var toAdd = new Box(position, 1, 1, 1, 1);
                 toAdd.IsAffectedByGravity = false;
+                toAdd.LinearVelocity = 3 * Vector3.Normalize(toAdd.Position);
                 Space.Add(toAdd);
+                addedEntities.Add(toAdd);
 
             }
 
@@ -53,21 +59,48 @@ namespace BEPUphysicsDemos.Demos.Extras.Tests
         }
 
         Random random = new Random();
+        private RawList<Entity> addedEntities = new RawList<Entity>();
+        private RawList<Entity> removedEntities = new RawList<Entity>();
+
         public override void Update(float dt)
         {
-            for (int i = 0; i < 20; i++)
+            for (int i = removedEntities.Count - 1; i >= 0; --i)
             {
-                var entity = Space.Entities[random.Next(Space.Entities.Count)];
-                Space.Remove(entity);
-                entity.Position = new Vector3(
-                            (float)random.NextDouble() * width,
-                            (float)random.NextDouble() * height,
-                            (float)random.NextDouble() * length);
-                Space.Add(entity);
+                if (random.NextDouble() < 0.2)
+                {
+                    var entity = removedEntities[i];
+                    addedEntities.Add(entity);
+                    Space.Add(entity);
+                    removedEntities.FastRemoveAt(i);
+                }
+            }
+            for (int i = addedEntities.Count - 1; i >= 0; --i)
+            {
+                if (random.NextDouble() < 0.02)
+                {
+                    var entity = addedEntities[i];
+                    removedEntities.Add(entity);
+                    Space.Remove(entity);
+                    addedEntities.FastRemoveAt(i);
+                }
+            }
+
+            if (Game.MouseInput.MiddleButton != Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    var entity = addedEntities[random.Next(addedEntities.Count)];
+                    entity.Position = new Vector3(
+                        (float)(random.NextDouble() - 0.5f) * width,
+                        (float)(random.NextDouble() - 0.5f) * height,
+                        (float)(random.NextDouble() - 0.5f) * length);
+                }
             }
             base.Update(dt);
-        }
 
+
+        }
+        
         /// <summary>
         /// Gets the name of the simulation.
         /// </summary>
