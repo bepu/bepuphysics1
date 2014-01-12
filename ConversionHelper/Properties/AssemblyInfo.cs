9077a4dc41384b5f39efcb7ba95b2712fdc081d1
Ross Nordby <Ross Nordby@localhost>
2014-01-12 17:46:42 -0600

    -BEPUik is now correctly included in the BEPUphysics.sln. -Updated project properties to v1.4.0 and dates to 2014.

--- ConversionHelper/Properties/AssemblyInfo.cs
+++ ConversionHelper/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ConversionHelper")]
 [assembly: AssemblyDescription("Simple math conversion helpers")]
 [assembly: AssemblyCompany("Bepu Entertainment, LLC")]
-[assembly: AssemblyCopyright("Copyright © 2013, Bepu Entertainment LLC")]
+[assembly: AssemblyCopyright("Copyright © 2014, Bepu Entertainment LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
