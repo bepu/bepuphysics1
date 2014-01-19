454f0d81ca3f0620a30c90cc7af9a9a6a26e6939
Ross Nordby <Ross Nordby@localhost>
2014-01-19 16:12:10 -0600

    -Fixed a very misleading/incorrect exception message.

--- BEPUphysicsDemos/BEPUphysicsDemos/Demos/Extras/Tests/ConvexHullTestDemo.cs
+++ BEPUphysicsDemos/BEPUphysicsDemos/Demos/Extras/Tests/ConvexHullTestDemo.cs
@@ -20,20 +20,19 @@ namespace BEPUphysicsDemos.Demos.Extras.Tests
             : base(game)
         {
 
-            var random = new Random(0);
+            var random = new Random(5);
 
             for (int i = 0; i < 500000; ++i)
             {
                 List<Vector3> points = new List<Vector3>();
-                for (int k = 0; k < random.Next(8, 30); k++)
+                for (int k = 0; k < random.Next(8, 60); k++)
                 {
-                    points.Add(new Vector3(3 * (float)random.NextDouble(), 5 * (float)random.NextDouble(), 3 * (float)random.NextDouble()));
+                    points.Add(new Vector3(-100 + 30 * (float)random.NextDouble(), 100 + 500 * (float)random.NextDouble(), 100 + 30 * (float)random.NextDouble()));
                 }
                 var convexHull = new ConvexHull(new Vector3(0, 7, 0), points, 10);
-                //Space.Add(convexHull);
+                Console.WriteLine(convexHull.CollisionInformation.Shape.Vertices.Count);
             }
-
-
+            
             var vertices = new[] 
             { 
                 new Vector3(0, -1.750886E-9f, -1.5f),
