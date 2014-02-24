4a1fa87c21048eb461f568bc6108a0444e3935c2
Ross Nordby <Ross Nordby@localhost>
2014-02-23 23:41:02 -0600

    -Added BufferPool<T>, LockingBufferPool<T>, and BufferPools<T> for reusable array management.

---
+++ BEPUutilities/ResourceManagement/BufferPools.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BEPUutilities.ResourceManagement
+{
+    /// <summary>
+    /// Contains locking and thread static buffer pools for the specified type.
+    /// </summary>
+    /// <typeparam name="T">Type of element in the buffers stored in the pools.</typeparam>
+    public class BufferPools<T>
+    {
+        /// <summary>
+        /// Gets a buffer pool for this type which provides thread safe resource acquisition and return.</summary>
+        public static LockingBufferPool<T> Locking { get; private set; }
+
+        [ThreadStatic]
+        private static BufferPool<T> threadPool;
+        /// <summary>
+        /// Gets the pool associated with this thread.
+        /// </summary>
+        public static BufferPool<T> Thread
+        {
+            get { return threadPool ?? (threadPool = new BufferPool<T>()); }
+        }
+
+        static BufferPools()
+        {
+            Locking = new LockingBufferPool<T>();
+        }
+    }
+}
---
+++ BEPUutilities/ResourceManagement/LockingBufferPool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BEPUutilities.ResourceManagement
+{
+    /// <summary>
+    /// Provides storage for reusable arrays with power-of-2 lengths.
+    /// Provides thread safe TakeFromPoolIndex, Take, and GiveBack implementations.
+    /// </summary>
+    /// <typeparam name="T">Type of resource contained in the buffers.</typeparam>
+    public class LockingBufferPool<T> : BufferPool<T>
+    {
+        private SpinLock locker = new SpinLock();
+
+        /// <summary>
+        /// Takes a buffer from the given pool index.
+        /// </summary>
+        /// <param name="poolIndex">Pool to grab a buffer from.</param>
+        /// <returns>Pool of the requested size.</returns>
+        public override T[] TakeFromPoolIndex(int poolIndex)
+        {
+            locker.Enter();
+            var toReturn = base.TakeFromPoolIndex(poolIndex);
+            locker.Exit();
+            return toReturn;
+        }
+        
+        /// <summary>
+        /// Releases a buffer back to the pool without clearing it out.
+        /// </summary>
+        /// <param name="buffer">Buffer to return to the pool.</param>
+        /// <param name="poolIndex">Pool index associated with the buffer.</param>
+        public override void GiveBack(T[] buffer, int poolIndex)
+        {
+            locker.Enter();
+            base.GiveBack(buffer, poolIndex);
+            locker.Exit();
+        }
+
+
+    }
+}
