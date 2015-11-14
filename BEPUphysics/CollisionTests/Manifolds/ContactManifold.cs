82eb9b3dc631531946fabe421663bda25566a89a
Ross Nordby <Ross Nordby@localhost>
2015-11-13 18:47:34 -0600

    Added a SimpleVoxelCollidableDemo, showing how to create a custom voxel grid collidable that collides with convex objects. Made few changes to make out-of-project pair handlers more convenient.

--- BEPUphysics/CollisionTests/Manifolds/ContactManifold.cs
+++ BEPUphysics/CollisionTests/Manifolds/ContactManifold.cs
@@ -24,6 +24,7 @@ namespace BEPUphysics.CollisionTests.Manifolds
                 return new ReadOnlyList<Contact>(contacts);
             }
         }
+
         protected UnsafeResourcePool<Contact> unusedContacts;
 
 
