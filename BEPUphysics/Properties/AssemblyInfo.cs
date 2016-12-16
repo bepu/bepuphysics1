a3979f05607c9813be1609ad86232e3b947879b7
Ross Nordby <Ross Nordby@localhost>
2016-12-16 16:47:09 -0600

    Pushing to 1.5.0 and preparing another release because why not.

--- BEPUphysics/Properties/AssemblyInfo.cs
+++ BEPUphysics/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
 #if WINDOWS_PHONE
 [assembly: CodeGeneration(CodeGenerationFlags.EnableFPIntrinsicsUsingSIMD)]
 #endif
